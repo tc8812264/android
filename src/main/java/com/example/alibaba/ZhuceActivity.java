cac8c3ab11f6875436fddd3470546fafe1b4b2ee
tc8812264 <807312447@qq.com>
2016-05-06 15:15:53 +0800

    update my android

---
+++ src/main/java/com/example/alibaba/ZhuceActivity.java
@@ -0,0 +1,13 @@
+package com.example.alibaba;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ZhuceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zhuce);
+    }
+}
